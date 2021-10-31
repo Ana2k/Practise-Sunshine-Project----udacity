2e572ba80b3798fdda6ad84729ae1b16d3911017
Ana2k <anushkachakra@gmail.com>
2021-10-31 11:37:51 +0530

    some basic nothing changes

--- app/src/main/java/com/example/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/sunshineapp/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //so far about Adb, command line tools and ./gradlew tasks(from root--> chmod first)
+
+    //https://classroom.udacity.com/courses/ud851/lessons/93affc67-3f0b-4f9b-b3a4-a7a26f241a86/concepts/98b2f686-aca1-4547-9632-31e84ccca355
+    //https://github.com/udacity/ud851-Sunshine/tree/student/S01.01-Exercise-CreateLayout/app/src/main/java/com/example/android/sunshine
+
 }
