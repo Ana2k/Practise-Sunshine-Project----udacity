674c04994fea1edca6564e351804deddd2718168
Ana2k <anushkachakra@gmail.com>
2021-10-31 12:23:16 +0530

    some basic setup and ui changes

--- app/src/main/java/com/example/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/sunshineapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.sunshineapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+
+        String [] toyNames = ToyBox.getToyNames();
+        for (String name: toyNames){
+            mToysListTextView.append(name+"\n\n");
+        }
     }
 }
