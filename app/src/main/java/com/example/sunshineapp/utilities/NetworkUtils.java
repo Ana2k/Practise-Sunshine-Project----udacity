4291434fd0dd066ca74164a7779366dbdebd3176
Ana2k <anushkachakra@gmail.com>
2021-11-03 10:24:19 +0530

    value files set up

--- app/src/main/java/com/example/sunshineapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/sunshineapp/utilities/NetworkUtils.java
@@ -1,5 +1,49 @@
 package com.example.sunshineapp.utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
-//    https://github.com/udacity/ud851-Sunshine/blob/student/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+    //URLS
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+    private static final String DYNAMIC_WEATHER_URL = "https://andfun-weather.udacity.com/weather";
+    private static final String STATIC_WEATHER_URL = "https://andfun-weather.udacity.com/staticweather";
+
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
+
+    private static final String format = "json";
+    private static final String units = "metric";
+    private static final int numDays = 14;
+
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
+
+    public static URL buildURL(Double lat, Double lon){
+        return null;
+    }
+    public static String getResponseHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }//samme code as toy_app_network :)
+    }
 }
---
+++ app/src/main/java/com/example/sunshineapp/utilities/SunshineDateUtils.java
@@ -0,0 +1,4 @@
+package com.example.sunshineapp.utilities;
+
+public class SunshineDateUtils {
+}
