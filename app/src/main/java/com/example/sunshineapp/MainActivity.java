68484c8d863a6e6e857ebc1d38f17d0abdb4ba5b
Ana2k <anushkachakra@gmail.com>
2021-12-06 21:58:32 +0530

    till before objects of loader manager

--- app/src/main/java/com/example/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/sunshineapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sunshineapp;
 
-import android.os.AsyncTask;
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
+import android.content.Loader;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +21,7 @@ import java.net.URL;
 //TODO
 //https://github.com/udacity/ud851-Exercises/blob/student/Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     private static final String SEARCH_QUERY_URL_EXETRA = "query";
     private static final String SEARCH_RESULTS_RAW_JSON = "results";
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mSearchResultsTextView, mErrorMessageDisplay, mUrlDisplayTextView;
     private EditText mSearchboxEditText;
     private ProgressBar mLoadingIndicator;
+
+    private final int GITHUB_SEARCH_LOADER = 13;
 
     /**
      * gets the "simple name" of the class MainActivity, which happens to be "MainActivity". One could just write
@@ -83,53 +88,71 @@ public class MainActivity extends AppCompatActivity {
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
 
-        //AsyncTask
-        new GithubQueryTask().execute(githubSearchUrl);
+        if(githubQuery.equals("") ||githubQuery==null || githubSearchUrl==null){
+            return;
+        }
+
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(SEARCH_QUERY_URL_EXETRA,githubSearchUrl.toString());
+
+
 
 
     }
 
-
-    private class GithubQueryTask extends AsyncTask<URL, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //loadingindicator is visible
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void onPostExecute(String githubSearchResults) {
-
-
-            //if data is not null show it by setting text
-            // else showErrorMessage.
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if((githubSearchResults!=null) && !githubSearchResults.equals("")){
-                showJsonDataView();
-                mSearchResultsTextView.setText(githubSearchResults);
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+            @Override
+            protected void onStartLoading(){
+                if(args==null){
+                    return;
+                }
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+                forceLoad();
             }
-            else{
-                showErrorMessage();
+            @Override
+            public String loadInBackground() {
+                //takes out the data using NetworkUtils.getReponseFromHttpUrl
+                String searchQueryUrlString = args.getString(SEARCH_QUERY_URL_EXETRA);
+                if (TextUtils.isEmpty(searchQueryUrlString)){
+                    return null;
+                }
+
+                try{
+                    URL githubUrl = new URL(searchQueryUrlString);
+                    String githubSearchResults = NetworkUtils.getResponseFromHttpUtl(githubUrl);
+                    return githubSearchResults;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+
             }
+        };
+    }
 
-
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        //if data is not null show it by setting text
+        // else showErrorMessage.
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+        if(data == null){
+            showErrorMessage();
         }
+        else{
 
-        @Override
-        protected String doInBackground(URL... urls) {
-            //takes out the data using NetworkUtils.getReponseFromHttpUrl
-            //
-            URL searchUrl = urls[0];
-            String githubSearchResults = null;
-            try{
-                githubSearchResults = NetworkUtils.getResponseFromHttpUtl(searchUrl);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return githubSearchResults;
-
+            showJsonDataView();
+            mSearchResultsTextView.setText(data);
         }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+        /*
+         * We aren't using this method in our example application, but we are required to Override
+         * it to implement the LoaderCallbacks<String> interface
+         */
     }
 
     private void showErrorMessage() {
