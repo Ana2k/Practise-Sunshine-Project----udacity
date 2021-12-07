8e57b9a90925afd671eeae94899e99b1bb04d22a
Ana2k <anushkachakra@gmail.com>
2021-12-07 22:39:46 +0530

    till before taking in raw

--- app/src/main/java/com/example/sunshineapp/AudioVisuals/AudioInputReader.java
+++ app/src/main/java/com/example/sunshineapp/AudioVisuals/AudioInputReader.java
@@ -1,5 +1,23 @@
 package com.example.sunshineapp.AudioVisuals;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
+
 public class AudioInputReader {
     private final VisualiserView mVisualiserView;
+    private final Context mContext;
+    private MediaPlayer mPlayer;
+    private Visualizer mVisualizer;
+
+
+    public AudioInputReader(VisualiserView visualiserView, Context context) {
+        this.mVisualiserView = visualiserView;
+        this.mContext = context;
+        initVisualiser();
+    }
+
+    private void initVisualiser() {
+
+    }
 }
