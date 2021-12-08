0fd46c48ef81c5a3fbe72024a1227d0e2cea1878
Ana2k <anushkachakra@gmail.com>
2021-12-08 09:07:24 +0530

    till fragment and preference formation

--- app/src/main/java/com/example/sunshineapp/SettingsActivity.java
+++ app/src/main/java/com/example/sunshineapp/SettingsActivity.java
@@ -1,8 +1,12 @@
 package com.example.sunshineapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getSupportActionBar();
+
+        //Set the action bar back button to look like an up button
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_settings){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
