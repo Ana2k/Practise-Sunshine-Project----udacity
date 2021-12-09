1d09569fef474eb0ca7dd60cb08f47225c182845
Ana2k <anushkachakra@gmail.com>
2021-12-09 12:39:18 +0530

    till setPreference summary completed

--- app/src/main/java/com/example/sunshineapp/SettingsFragment.java
+++ app/src/main/java/com/example/sunshineapp/SettingsFragment.java
@@ -3,9 +3,12 @@ package com.example.sunshineapp;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+//1)PreferenceScreen preferenceScreen = getPreferenceScreen();
 
 //(2) Create a setPreferenceSummary which takes a Preference and String value as parameters.
 // This method should check if the preference is a ListPreference and, if so, find the label
@@ -33,19 +36,69 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualiser);
+
+        // Get the preference screen, get the number of preferences and iterate through
+        // all of the preferences if it is not a checkbox preference, call the setSummary method
+        // passing in a preference and the value of the preference
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
+
+        int count = preferenceScreen.getPreferenceCount();
+
+        // Go through all of the preferences, and set up their preference summary.
+        for(int i=0;i<count;i+=1){
+            Preference currentPreference = preferenceScreen.getPreference(i);
+            // You don't need to set up preference summaries for checkbox preferences because
+            // they are already set up in xml using summaryOff and summary On
+            if(!(currentPreference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(currentPreference.getKey(),"");
+                setPreferenceSummary(currentPreference,value);
+            }
+        }
+        //we create an empty preference summary just for
+        //the listPreference
     }
 
+
+    // Override onSharedPreferenceChanged and, if it is not a checkbox preference,
+    // call setPreferenceSummary on the changed preference
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
+        // Figure out which preference was changed
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            // Updates the summary for the preference
+            if(!(preference instanceof CheckBoxPreference)){
+                String value = sharedPreferences.getString(preference.getKey(),"");
+                setPreferenceSummary(preference,value);
+            }
+        }
+        //we attatch that value to correct preferences
     }
 
+    // This method should check if the preference is a ListPreference and, if so, find the label
+    // associated with the value. You can do this by using the findIndexOfValue and getEntries methods
+    // of Preference.
+    /**
+     * Updates the summary for the preference
+     *
+     * @param preference The preference to be updated
+     * @param value      The value that the preference was updated to
+     */
     public void setPreferenceSummary(Preference preference, String value) {
         if (preference instanceof ListPreference) {
             ListPreference listPreference = (ListPreference) preference;
@@ -56,4 +109,5 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
         }
 
     }
+
 }
