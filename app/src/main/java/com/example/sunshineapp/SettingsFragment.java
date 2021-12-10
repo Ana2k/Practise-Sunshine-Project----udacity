aebb238dfda1ef0aabcb0ecea07698935e04b08b
Ana2k <anushkachakra@gmail.com>
2021-12-10 13:00:10 +0530

    till before checkboxpreference are changed

--- app/src/main/java/com/example/sunshineapp/SettingsFragment.java
+++ app/src/main/java/com/example/sunshineapp/SettingsFragment.java
@@ -2,8 +2,10 @@ package com.example.sunshineapp;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
 import androidx.preference.CheckBoxPreference;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
@@ -11,14 +13,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.List;
-
-public class SettingsFragment extends PreferenceFragmentCompat {
-
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     @Override
@@ -44,11 +39,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         SharedPreferences sharedPreferences = preferenceScreen.getSharedPreferences();
 
         int count = preferenceScreen.getPreferenceCount();
-        for(int i =  0;i<count;i++){
+        for (int i = 0; i < count; i++) {
             Preference currentPreference = preferenceScreen.getPreference(i);
-            if(!(currentPreference instanceof CheckBoxPreference)){
-                String value = sharedPreferences.getString(currentPreference.getKey(),"");
-                setPreferenceSummary(currentPreference,value);
+            if (!(currentPreference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(currentPreference.getKey(), "");
+                setPreferenceSummary(currentPreference, value);
             }
         }
 
@@ -60,14 +55,27 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
-    public void setPreferenceSummary(Preference preference, String value){
-        ListPreference listPreference = (ListPreference) preference;
-        int prefIndex = listPreference.findIndexOfValue(value);
-        if(prefIndex>=0){
-            listPreference.setSummary(listPreference.getEntries()[prefIndex]);
-        }else if(preference instanceof EditTextPreference){
-            preference.setSummary(value);
+
+    public void setPreferenceSummary(Preference preference, String value) {
+        String stringValue = preference.toString();
+        String key = preference.getKey();
+
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
         }
+        else{
+            preference.setSummary(stringValue);
+        }
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
     }
 }
 
