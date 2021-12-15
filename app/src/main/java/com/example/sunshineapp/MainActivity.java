c6d4edfefd3939ad3ecb04140006bb9313f06ec4
Ana2k <anushkachakra@gmail.com>
2021-12-15 10:46:52 +0530

    asynctask todos

--- app/src/main/java/com/example/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/sunshineapp/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.sunshineapp;
 import android.annotation.SuppressLint;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -22,6 +25,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.droidtermsprovider.DroidTermsExampleContract;
 import com.example.sunshineapp.data.SunshinePreferences;
 import com.example.sunshineapp.utilities.NetworkUtils;
 import com.example.sunshineapp.utilities.OpenWeatherJsonUtils;
@@ -36,14 +40,39 @@ import java.net.URL;
 //--\/
 
 public class MainActivity extends AppCompatActivity {
+
+    private Cursor mData;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        new WordFetchTask().execute();
     }
 
     public void onButtonClick(View view) {
 
+    }
+    //from network branch of sunshine appp
+    private class WordFetchTask extends AsyncTask<Void,Void, Cursor>{
+
+        @Override
+        protected void onPostExecute(Cursor cursor) {
+            super.onPostExecute(cursor);
+            mData = cursor;
+
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            //write the code to access the droidTermsExample return cursor objects
+            ContentResolver resolver = getContentResolver();
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                Cursor cursor = resolver.query(DroidTermsExampleContract.CONTENT_URI,null,null,null);
+                return cursor;
+            }
+
+            return null;
+
+        }
     }
 }
 
